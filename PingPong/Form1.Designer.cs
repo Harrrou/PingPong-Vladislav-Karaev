304ac45357609ef822ab74213a9c68ce9e469c67
Влад Караев <vlad-karaev-2014@mail.ru>
2020-04-08 15:42:59 +0400

    Добавьте файлы проекта.

---
+++ PingPong/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace PingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Background = new System.Windows.Forms.Panel();
+            this.gamepanel = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Background.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gamepanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Background
+            // 
+            this.Background.Controls.Add(this.ball);
+            this.Background.Controls.Add(this.gamepanel);
+            this.Background.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Background.Location = new System.Drawing.Point(0, 0);
+            this.Background.Name = "Background";
+            this.Background.Size = new System.Drawing.Size(800, 450);
+            this.Background.TabIndex = 0;
+            // 
+            // gamepanel
+            // 
+            this.gamepanel.BackColor = System.Drawing.Color.Aquamarine;
+            this.gamepanel.Location = new System.Drawing.Point(333, 425);
+            this.gamepanel.Name = "gamepanel";
+            this.gamepanel.Size = new System.Drawing.Size(192, 13);
+            this.gamepanel.TabIndex = 0;
+            this.gamepanel.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Purple;
+            this.ball.Location = new System.Drawing.Point(381, 108);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(25, 25);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Background.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gamepanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Background;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox gamepanel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ PingPong/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {    
+        private int speed_vertical = 4;
+        private int speed_hor = 4;
+        private int score = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Enabled = true;
+
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            gamepanel.Top = Background.Bottom - (Background.Bottom / 10);
+
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            gamepanel.Left = Cursor.Position.X - (gamepanel.Width / 2);
+
+            ball.Left += speed_hor;
+            ball.Top += speed_vertical;
+
+            if (ball.Left <= Background.Left)
+                speed_hor *= -1;
+
+
+            if (ball.Right >= Background.Right)
+                speed_hor *= -1;
+
+
+            if (ball.Top <= Background.Top)
+                speed_vertical *= -1;
+
+            if (ball.Bottom >= Background.Bottom)
+                timer.Enabled = false;
+
+            if (ball.Bottom >= gamepanel.Top && ball.Bottom <= gamepanel.Bottom && ball.Left >= gamepanel.Left && ball.Right <= gamepanel.Right)
+            {
+                speed_hor += 3;
+                speed_vertical += 3;
+                speed_vertical *= -1;
+                score += 1;
+
+            }
+        }
+    }
+}
